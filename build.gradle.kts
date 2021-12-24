ec805b8d1e9d211a12966d3508e945b2a6771914
Shoothzj <shoothzj@gmail.com>
2021-12-24 14:06:27 +0800

    update depdency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,12 @@ dependencies {
     implementation("io.netty:netty-common:4.1.70.Final")
     implementation("com.google.guava:guava:31.0.1-jre")
     implementation("org.hibernate.validator:hibernate-validator:7.0.1.Final")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
+    testCompileOnly("org.projectlombok:lombok:1.18.22")
+    testImplementation("org.apache.pulsar:pulsar-broker:2.9.1")
     testImplementation("org.glassfish:jakarta.el:4.0.2")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
