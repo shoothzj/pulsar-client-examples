76336c097794960e262800c0b710e02fbbafa8bc
ZhangJian He <shoothzj@gmail.com>
2022-07-12 17:00:27 +0800

    Bump misc dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,22 +10,27 @@ repositories {
     mavenCentral()
 }
 
+val junitVersion = "5.8.2"
+val lombokVersion = "1.18.24"
+val log4jVersion = "2.18.0"
+val pulsarVersion = "2.10.1"
+
 dependencies {
-    annotationProcessor("org.projectlombok:lombok:1.18.24")
-    compileOnly("org.projectlombok:lombok:1.18.22")
-    implementation("org.apache.pulsar:pulsar-client-admin-original:2.10.0")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    implementation("org.apache.pulsar:pulsar-client-admin-original:$pulsarVersion")
     implementation("com.github.ben-manes.caffeine:caffeine:2.9.3")
     implementation("io.netty:netty-common:4.1.77.Final")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("org.hibernate.validator:hibernate-validator:7.0.1.Final")
-    implementation("org.apache.logging.log4j:log4j-core:2.17.2")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.2")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.22")
-    testCompileOnly("org.projectlombok:lombok:1.18.22")
-    testImplementation("org.apache.pulsar:pulsar-proxy:2.10.1") {
+    implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testImplementation("org.apache.pulsar:pulsar-proxy:$pulsarVersion") {
         exclude("org.slf4j", "slf4j-log4j12")
     }
-    testImplementation("org.apache.pulsar:pulsar-broker:2.10.1") {
+    testImplementation("org.apache.pulsar:pulsar-broker:$pulsarVersion") {
         exclude("org.slf4j", "slf4j-log4j12")
     }
     testImplementation("org.apache.bookkeeper:bookkeeper-server:4.14.4") {
@@ -33,8 +38,8 @@ dependencies {
     }
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.glassfish:jakarta.el:4.0.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 tasks.getByName<Test>("test") {
